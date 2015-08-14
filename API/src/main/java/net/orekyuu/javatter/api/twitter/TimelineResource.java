e698f1deb7fff301d83cef14c18705d5fad6d409
orekyuu <orekyuu@gmail.com>
2015-08-14 16:06:43 +0900

    Javadocを書いた

--- API/src/main/java/net/orekyuu/javatter/api/twitter/TimelineResource.java
+++ API/src/main/java/net/orekyuu/javatter/api/twitter/TimelineResource.java
@@ -3,7 +3,14 @@ package net.orekyuu.javatter.api.twitter;
 import com.gs.collections.api.list.MutableList;
 import net.orekyuu.javatter.api.twitter.model.Tweet;
 
+/**
+ * タイムラインを取得する操作
+ */
 public interface TimelineResource {
 
+    /**
+     * タイムラインを公式APIから取得します。
+     * @return 取得したツイート
+     */
     MutableList<Tweet> getHomeTimeline();
 }
