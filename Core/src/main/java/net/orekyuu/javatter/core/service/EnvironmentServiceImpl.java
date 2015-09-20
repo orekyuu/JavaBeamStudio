b50d97fe9de46a3cb35c241548550098c374de90
orekyuu <orekyuu@gmail.com>
2015-09-20 23:52:48 +0900

    バージョンを更新

--- Core/src/main/java/net/orekyuu/javatter/core/service/EnvironmentServiceImpl.java
+++ Core/src/main/java/net/orekyuu/javatter/core/service/EnvironmentServiceImpl.java
@@ -5,11 +5,11 @@ import net.orekyuu.javatter.api.service.EnvironmentService;
 public class EnvironmentServiceImpl implements EnvironmentService {
     @Override
     public String getJavaBeamStudioVersion() {
-        return "1.0.0";
+        return "1.0.1";
     }
 
     @Override
     public String getJavaBeamStudioApiVersion() {
-        return "1.0.0";
+        return "1.0.1";
     }
 }
